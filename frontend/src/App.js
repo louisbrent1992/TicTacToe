41c3932fc3f4dfea1672ef8de8f2215a234673a2
Louis Brent <louisbrent1992@gmail.com>
2022-01-26 17:16:41 -0800

    fixed files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,10 @@
-import React from "react";
-import "./App.css";
-import { averageScore } from "./Utils/functions";
-const data = require("./Data/db.json");
+
+
 
 function App() {
   return (
     <div>
       <h1>Hello World!</h1>
-      <h2>Student Scores</h2>
-      <p>Average Score = {averageScore(data)}</p>
     </div>
   );
 }
